827a2cda646b145158058b4727485e67968ef196
Evers, Martijn <mevers@gk-software.com>
2024-11-21 10:21:28 +0100

    Do not panic when initialize is called without cgo

--- clipboard_nocgo.go
+++ clipboard_nocgo.go
@@ -2,10 +2,13 @@
 
 package clipboard
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 func initialize() error {
-	panic("clipboard: cannot use when CGO_ENABLED=0")
+	return errors.New("clipboard: cannot use when CGO_ENABLED=0")
 }
 
 func read(t Format) (buf []byte, err error) {
